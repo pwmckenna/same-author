6b41061ccbda9abb7adf4d0082a45d8c04ed31d1
Patrick Williams <patrickw@porch.com>
2016-01-07 14:19:25 -0800

    add debugging and tests

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import path from 'path';
 import getPkgRepo from 'get-pkg-repo';
 import findRoot from 'find-root';
+import debugLib from 'debug';
+
+const debug = debugLib('same-author');
 
 function author(codePath) {
   const root = findRoot(codePath);
-  const pkg = require(path.resolve(root, 'package.json'));
+  const pkgPath = path.resolve(root, 'package.json');
+  const pkg = require(pkgPath);
   const repo = getPkgRepo(pkg);
-  const user = repo.user;
-  return user;
+  const author = repo.user;
+  debug('author: %s, %s', codePath, author);
+  return author;
 }
 
-export default function (codePath1, codePath2) {
+export default function (codePath1, otherPath) {
+  const codePath2 = otherPath || process.cwd();
   try {
-    return author(codePath1) === author(codePath2 || process.cwd());
+    const author1 = author(codePath1);
+    const author2 = author(codePath2);
+    debug('authors: %s, %s', author1, author2);
+    return author1 === author2;
   } catch (err) {
+    debug('error: %o', err);
     return false;
   }
 }
